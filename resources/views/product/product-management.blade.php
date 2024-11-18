f02da10c75f4344dc2df4281049263e68c22c864
Duy271 <Nguyenkhanhduy.learn@gmail.com>
2024-11-18 09:55:36 +0700

    Tpl sign

---
+++ resources/views/product/product-management.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.index')
+@section('title','Báo Cáo Doanh Thu')
+@section('css')
+<style>
+   
+</style>
+@endsection
+@section('content')
+    ahhhhhhhhh
+@endsection
+@section('js')
+
+@endsection
