aa59bd786af3a0f83460dc0f1053fbf1bf1a081f
Duy271 <nguyenkhanhduy.learn@gmail.com>
2024-12-12 18:57:48 +0700

    Permission 12/12

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,9 @@ class Product extends Model
 {
     use HasFactory;
     protected $table = 'product';
-
+    protected $primaryKey='id';
+    protected $keyType = 'string';
+    public $incrementing = false;
     // Các thuộc tính có thể gán
     protected $fillable = [
         'ID_SupCategory',
